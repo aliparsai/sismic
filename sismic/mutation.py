815bb6c0bb15985c8c00914f9c7027bed9877382
Ali Parsai <ali.parsai@email.com>
2016-01-24 14:50:22 +0100

    early version of random stories generator using mutation testing

--- sismic/mutation.py
+++ sismic/mutation.py
@@ -1,6 +1,8 @@
 from sismic import model
 from sismic import stories
 from sismic import interpreter
+from sismic import exceptions
+
 from copy import deepcopy
 
 
@@ -31,15 +33,70 @@ class Mutation(object):
         return [mutant for mutant in self.mutants if self.mutant_status[mutant] == "Survived"]
 
     def evaluate_story(self, story: stories.Story):
-        original_trace = story.tell(interpreter.Interpreter(self.statechart)).trace
+
+        context_original = list()
+        configuration_original = list()
+        trace_original = list()
+        time_original = list()
+        steps_original = list()
+
+        original_interpreter = interpreter.Interpreter(self.statechart)
+        for item, step_original in story.tell_by_step(original_interpreter):
+            steps_original.append(step_original)
+            context_original.append(original_interpreter.context)
+            configuration_original.append(original_interpreter.configuration)
+            trace_original.append(original_interpreter.trace)
+            time_original.append(original_interpreter.time)
+
+        # tell_story_original = story.tell(interpreter.Interpreter(self.statechart))
+        # assert isinstance(tell_story_original, interpreter.Interpreter)
+
         kill_count = 0
 
         for mutant in self.survived_mutants:
-            mutant_trace = story.tell(interpreter.Interpreter(mutant)).trace
+            counter = -1
+            mutant_interpreter = interpreter.Interpreter(mutant)
 
-            if mutant_trace != original_trace:
-                self.mutant_status[mutant] = "Killed"
-                kill_count += 1
+            for item, step_mutant in story.tell_by_step(mutant_interpreter):
+                counter += 1
+
+                if steps_original[counter] != step_mutant:
+                    self.mutant_status[mutant] = "Killed"
+                    kill_count += 1
+                    break
+
+                if context_original[counter] != mutant_interpreter.context:
+                    self.mutant_status[mutant] = "Killed"
+                    kill_count += 1
+                    break
+
+                if configuration_original[counter] != mutant_interpreter.configuration:
+                    self.mutant_status[mutant] = "Killed"
+                    kill_count += 1
+                    break
+
+                if trace_original[counter] != mutant_interpreter.trace:
+                    self.mutant_status[mutant] = "Killed"
+                    kill_count += 1
+                    break
+
+                if time_original[counter] != mutant_interpreter.time:
+                    self.mutant_status[mutant] = "Killed"
+                    kill_count += 1
+                    break
+
+
+            # tell_story_mutant = story.tell(interpreter.Interpreter(mutant))
+            # assert isinstance(tell_story_mutant, interpreter.Interpreter)
+
+            # except exceptions.NonDeterminismError as err:
+            #     print(err)
+            #     print(mutant.mutator_type)
+            #     print(mutant.states)
+
+            # if tell_story_original.trace != tell_story_mutant.trace or tell_story_original.configuration != tell_story_mutant.configuration or tell_story_original.context != tell_story_mutant.context:
+            #     self.mutant_status[mutant] = "Killed"
+            #     kill_count += 1
 
         return kill_count
 
@@ -60,7 +117,7 @@ class Mutator(object):
 class StateMissing(Mutator):
     def __init__(self, statechart: model.Statechart = None):
         super().__init__(statechart=statechart)
-    
+
     def mutate(self) -> list:
         mutants = list()
 
@@ -82,8 +139,8 @@ class StateMissing(Mutator):
             #
             #     except:
             #         continue
-                    # print("\nERROR: \n", err)
-                    # print("\nLIST: \n", transition_list)
+            # print("\nERROR: \n", err)
+            # print("\nLIST: \n", transition_list)
 
             try:
                 new_mutant.remove_state(state_key)
@@ -100,7 +157,7 @@ class StateMissing(Mutator):
 class ArcMissing(Mutator):
     def __init__(self, statechart: model.Statechart = None):
         super().__init__(statechart=statechart)
-    
+
     def mutate(self) -> list:
         mutants = list()
 
@@ -127,7 +184,7 @@ class WrongStartingState(Mutator):
 
     def mutate(self) -> list:
         mutants = list()
-        top_level_states = [ state for state in self.statechart.states if self.statechart.parent_for(state) is None ]
+        top_level_states = [state for state in self.statechart.states if self.statechart.parent_for(state) is None]
         # state_names = set(self.statechart._children.keys())
         # print(top_level_states)
 
@@ -164,4 +221,3 @@ class WrongStartingState(Mutator):
                     mutants.append(new_mutant)
 
         return mutants
-
--- sismic/stories.py
+++ sismic/stories.py
@@ -2,6 +2,7 @@ from sismic.model import Event, InternalEvent
 from sismic import mutation
 
 import random
+import itertools
 
 __all__ = ['Pause', 'Story', 'random_stories_generator', 'story_from_trace']
 
@@ -93,7 +94,6 @@ def random_stories_generator(items, length: int=None, number: int=None):
         number -= 1
 
 
-
 def random_stories_generator_using_mutation(statechart, items, minimum_length: int=3):
     """
     A generator that returns random stories whose elements come from *items*.
@@ -115,31 +115,45 @@ def random_stories_generator_using_mutation(statechart, items, minimum_length: i
     discarded = 0
 
     while len(mutation_instance.survived_mutants) > 0:
-        if no_new_mutants_killed > length**2:
-            length += 1
-            no_new_mutants_killed = 0
+        # if length > 8:
+        #     break
 
-        # print("Current Length: ", length, " Discarded: ", discarded, " Kept: ", len(story_list),
-        #       " Remaining Mutants: ", len(mutation_instance.survived_mutants))
+        all_possible_stories = list(itertools.combinations_with_replacement(items, length))
+        random.shuffle(all_possible_stories)
 
-        story = Story()
-        for i in range(length):
-            story.append(random.choice(items))
-
-        if mutation_instance.evaluate_story(story) > 0:
-            story_list.append(story)
-            no_new_mutants_killed = 0
-
-        else:
-            discarded += 1
-            no_new_mutants_killed += 1
+        length += 1
 
 
-        if length > 20:
-            break
+        #
+        # if no_new_mutants_killed > length**2:
+        #     length += 1
+        #     no_new_mutants_killed = 0
+
+        print("Current Length: ", length - 1, " Discarded: ", discarded, " Kept: ", len(story_list),
+              " Remaining Mutants: ", len(mutation_instance.survived_mutants))
+
+        for item_list in all_possible_stories:
+            story = Story()
+            random.shuffle(list(item_list))
+            story.extend(item_list)
+
+        # for i in range(length):
+        #     story.append(random.choice(items))
+
+
+
+            if mutation_instance.evaluate_story(story) > 0:
+                story_list.append(story)
+                # no_new_mutants_killed = 0
+
+            else:
+                discarded += 1
+                # no_new_mutants_killed += 1
+
+        print(story)
 
     for i in mutation_instance.survived_mutants:
-        print()
+        print(i.mutator_type)
 
     return story_list
 
